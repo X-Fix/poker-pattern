e5f3e978df99e11704769380513366e57c99d8b4
X-Fix <cameron.stroebel08@gmail.com>
2020-03-03 00:20:40 +0200

    Issue #14: Prefer findByText

--- cypress/integration/navigation.js
+++ cypress/integration/navigation.js
@@ -1,14 +1,14 @@
 describe('Route navigation', () => {
     it('should be able to traverse the various routes', () => {
         cy.visit('http://localhost:5000');
-        cy.queryByTestId('link-atoms').click();
-        cy.findByTestId('link-icons')
+        cy.findByText('Atoms >').click();
+        cy.findByText('Icons')
             .click()
             .go('back');
-        cy.findByTestId('link-home').click();
-        cy.findByTestId('link-molecules').click();
-        cy.findByTestId('link-home').click();
-        cy.findByTestId('link-organisms').click();
-        cy.findByTestId('link-home').click();
+        cy.findByText('< Back').click();
+        cy.findByText('Molecules >').click();
+        cy.findByText('< Back').click();
+        cy.findByText('Organisms >').click();
+        cy.findByText('< Back').click();
     });
 });
