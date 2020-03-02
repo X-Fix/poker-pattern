4e690106ccc3f49e851a69e6dd3193fbfe7632a7
X-Fix <cameron.stroebel08@gmail.com>
2020-03-03 00:14:27 +0200

    Issue #14: Install helpful commands

--- cypress/integration/navigation.js
+++ cypress/integration/navigation.js
@@ -1,13 +1,14 @@
 describe('Route navigation', () => {
     it('should be able to traverse the various routes', () => {
         cy.visit('http://localhost:5000');
-        cy.get('[data-testid="link-atoms"]').click();
-        cy.get('[data-testid="link-icons"]').click();
-        cy.go('back');
-        cy.get('[data-testid="link-home"]').click();
-        cy.get('[data-testid="link-molecules"]').click();
-        cy.get('[data-testid="link-home"]').click();
-        cy.get('[data-testid="link-organisms"]').click();
-        cy.get('[data-testid="link-home"]').click();
+        cy.queryByTestId('link-atoms').click();
+        cy.findByTestId('link-icons')
+            .click()
+            .go('back');
+        cy.findByTestId('link-home').click();
+        cy.findByTestId('link-molecules').click();
+        cy.findByTestId('link-home').click();
+        cy.findByTestId('link-organisms').click();
+        cy.findByTestId('link-home').click();
     });
 });
