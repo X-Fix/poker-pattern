8b93974d9bad344ccd745c5d273fc23072094d47
X-Fix <cameron.stroebel08@gmail.com>
2020-03-03 00:34:10 +0200

    Issue #14: Add tests fro deep-links

--- cypress/integration/navigation.js
+++ cypress/integration/navigation.js
@@ -1,6 +1,6 @@
 describe('Route navigation', () => {
     it('should be able to traverse the various routes', () => {
-        cy.visit('http://localhost:5000');
+        cy.visit('/');
         cy.findByText('Atoms >').click();
         cy.findByText('Icons')
             .click()
@@ -10,5 +10,19 @@ describe('Route navigation', () => {
         cy.findByText('< Back').click();
         cy.findByText('Organisms >').click();
         cy.findByText('< Back').click();
+    });
+
+    it('should be able to start from any recognisable route', () => {
+        cy.visit('/atoms/icons');
+        cy.get('article').within(() => {
+            cy.get('h1')
+                .findByText('Icons')
+                .should('exist');
+        });
+
+        cy.visit('/molecules');
+        cy.findByText('< Back').click();
+        cy.findByText('Organisms >').click();
+        cy.url().should('include', '/organisms');
     });
 });
