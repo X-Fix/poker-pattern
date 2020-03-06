0bdf44cd16c950d30736607d6ea7e0d9d982d444
X-Fix <cameron.stroebel08@gmail.com>
2020-03-06 14:01:26 +0200

    Issue #19: Add styles and implementation details for text-input and radio

---
+++ src/02-molecules/InputsReference.svelte
@@ -0,0 +1,57 @@
+<script>
+    import ReferenceSheet from '../components/ReferenceSheet.svelte';
+    import ReferenceSection from '../components/ReferenceSection.svelte';
+    import ReferenceCode from '../components/ReferenceCode.svelte';
+</script>
+
+<style>
+
+</style>
+
+<ReferenceSheet heading="Inputs">
+    <p>
+        Input components take user input and translate it to a usable action or
+        data, eg. providing string value(s) or toggling/switching between states
+    </p>
+    <ReferenceSection heading="Text Input">
+        <p>
+            All text inputs will come with a descriptive label, and a
+            placeholder showing an example of expected input. The label,
+            placeholder, and input text all use title size font
+        </p>
+        <label class="text-input">
+            <p class="text-input__label">Your Name</p>
+            <input
+                class="text-input__text"
+                type="text"
+                placeholder="Eg. ‘John Johnson’" />
+        </label>
+        <ReferenceCode>
+            &lt;label class="text-input">&lt;p class="text-input__label">Your
+            Name&lt;/p>&lt;input class="text-input__text" type="text"
+            placeholder="Eg. ‘John Johnson’" />&lt;/label>
+        </ReferenceCode>
+    </ReferenceSection>
+
+    <ReferenceSection heading="Radio Input">
+        <p>
+            Radio inputs represent a list of "pick one" options. All options in
+            the same group need to have the same `name` attribute
+        </p>
+        <label class="radio">
+            <input type="radio" name="example" checked />
+            <span class="radio__circle"></span>
+            <p class="radio__label">Click me</p>
+        </label>
+        <label class="radio">
+            <input type="radio" name="example" />
+            <span class="radio__circle"></span>
+            <p class="radio__label">Click me</p>
+        </label>
+        <ReferenceCode>
+            &lt;label class="radio">&lt;input type="radio" name="example"
+            />&lt;span class="radio__circle">&lt;/span>&lt;p
+            class="radio__label">Click me&lt;/p>&lt;/label>
+        </ReferenceCode>
+    </ReferenceSection>
+</ReferenceSheet>
