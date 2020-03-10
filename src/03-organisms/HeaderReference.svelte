82331f9c05305f740c9ee96c1a303ea6f9107966
X-Fix <cameron.stroebel08@gmail.com>
2020-03-10 13:16:34 +0200

    Issue #23: Add header styles and reference

--- src/03-organisms/HeaderReference.svelte
+++ src/03-organisms/HeaderReference.svelte
@@ -8,4 +8,39 @@
 
 </style>
 
-<ReferenceSheet heading="Header" />
+<ReferenceSheet heading="Header">
+    <p>
+        The main header appears at the top of the page at all times. Before
+        joining or creating a session, the header buttons are not visible as
+        their options do not apply (this will be handled programmatically)
+    </p>
+    <p>
+        The header's buttons and spacing adjust as the screen gets larger. At
+        smaller screens, the buttons are just icons, and at the largest screen
+        sizes the buttons receive outter padding to maintain a maximum width
+    </p>
+    <ReferenceSection heading="Default">
+        <p>
+            There is only one style of heading. Resize this webpage to see the
+            various header versions
+        </p>
+
+        <header class="header">
+            <section class="header__content">
+                <button class="button button--header">
+                    <svg class="button__icon icon" aria-hidden="true">
+                        <use xlink:href="#menu"></use>
+                    </svg>
+                    <span class="button__text font-body">Menu</span>
+                </button>
+                <h1 class="header__heading font-title">Poker Planning</h1>
+                <button class="button button--header">
+                    <svg class="button__icon icon" aria-hidden="true">
+                        <use xlink:href="#chat"></use>
+                    </svg>
+                    <span class="button__text font-body">Chat</span>
+                </button>
+            </section>
+        </header>
+    </ReferenceSection>
+</ReferenceSheet>
