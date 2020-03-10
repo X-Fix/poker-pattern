c63f712c24b8220676e4914963872fcd0674c296
X-Fix <cameron.stroebel08@gmail.com>
2020-03-10 19:13:17 +0200

    Issue #25: Add Choose Form styles and reference

--- src/03-organisms/ChooseFormReference.svelte
+++ src/03-organisms/ChooseFormReference.svelte
@@ -9,5 +9,37 @@
 </style>
 
 <ReferenceSheet heading="Choose Form">
-    <!--  -->
+    <p>
+        The choose form allows the user to select whether to create a new
+        session or join an existing session
+    </p>
+
+    <ReferenceSection heading="Default">
+        <p>
+            There is only one type of choose form which will change its
+            orientation based on the screen size (portrait on smaller screens,
+            landscape on larger screens)
+        </p>
+        <div style="height: 600px;">
+            <form onsubmit="return false;" class="choose-form">
+                <button
+                    class="choose-form__create-session button button--square">
+                    <svg
+                        class="button__icon icon icon--huge"
+                        aria-hidden="true">
+                        <use xlink:href="#create"></use>
+                    </svg>
+                    <span class="button__text font-body">Create Session</span>
+                </button>
+                <button class="choose-form__join-session button button--square">
+                    <svg
+                        class="button__icon icon icon--huge"
+                        aria-hidden="true">
+                        <use xlink:href="#join"></use>
+                    </svg>
+                    <span class="button__text font-body">Join Session</span>
+                </button>
+            </form>
+        </div>
+    </ReferenceSection>
 </ReferenceSheet>
